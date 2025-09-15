ae4eca128bc7197f555f59408248b17972fbdc5b
Yukti <yuktimarwahgithub@gmail.com>
2025-09-15 14:59:02 +0530

    2nd lec added

---
+++ lec_2_data_types/Index.java
@@ -0,0 +1,15 @@
+public class Index{
+    public static void main(String[] args){
+        int a=10;
+        float b= 23.34f;
+        char c = '@';
+        String str = "RamSiya";
+        boolean flag = true;
+
+        System.out.println("integer a = "+a);
+        System.out.println("float b = "+b);
+        System.out.println("char c = "+c);
+        System.out.println("string str = "+str);
+        System.out.println("flag  = "+flag);
+    }
+}
