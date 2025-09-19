596359cb1238e596c9728df488c5a4e2602d1e8b
Yukti <yuktimarwahgithub@gmail.com>
2025-09-19 16:13:15 +0530

    6th lec added

---
+++ lec_06_switch_case/Switch.java
@@ -0,0 +1,24 @@
+public class Switch {
+    public static void main(String[] args){
+        int day = 4;
+        switch(day){
+            case 1: System.out.println("Monday");
+            break;
+            case 2: System.out.println("tuesday");
+            break;
+            case 3: System.out.println("wednesday");
+            break;
+            case 4: System.out.println("thursday");
+            break;
+            case 5: System.out.println("friday");
+            break;
+            case 6: System.out.println("saturday");
+            break;
+            case 7: System.out.println("sunday");
+            break;
+            default: System.out.println("Invalid day");
+            break;
+
+        }
+    }
+}
