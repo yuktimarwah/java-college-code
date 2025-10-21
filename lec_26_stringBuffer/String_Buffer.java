680cc04b8194266abdeed5751f1e0c7a5c4a96ac
Yukti <yuktimarwahgithub@gmail.com>
2025-10-21 19:31:12 +0530

    26th lec added

---
+++ lec_26_stringBuffer/String_Buffer.java
@@ -0,0 +1,11 @@
+
+public class String_Buffer {
+    public static void main(String[] args){
+    StringBuffer sb = new StringBuffer("java ");
+    System.out.println(sb.append("Programming"));
+    System.out.println(sb.insert(5,"Advance"));
+    System.out.println(sb.replace(0,5,"python"));
+    System.out.println(sb.charAt(0));
+    }
+    
+}
