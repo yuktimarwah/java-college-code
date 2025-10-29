38a2e1d233021a1f81acccb841dc54bafe59706d
Yukti <yuktimarwahgithub@gmail.com>
2025-10-29 18:03:21 +0530

    32nd lec added

---
+++ lec_32_merge_LL_21/Merge_LL.java
@@ -0,0 +1,26 @@
+package in class code.lec_32_merge_LL_21;
+
+public class Merge_LL {
+    public ListNode mergeTwoLists(ListNode l1,ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        if(l1!=null) curr.next = l1;
+        if(l2!=null) curr.next = l2;
+
+        return dummy.next;
+
+    }
+    
+}
