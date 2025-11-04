3ac6785746cfc7edd36a10f7c6cb0dc6c39cdc22
Yukti <yuktimarwahgithub@gmail.com>
2025-11-04 20:39:22 +0530

    36th lec added

---
+++ lec_36_FractionClass/FractionClass.java
@@ -0,0 +1,11 @@
+package in class code.lec_36_FractionClass;
+
+public class FractionClass {
+    public static class Fraction{
+        int nume;
+        int deno;
+    }
+    public static void main(String[] args) {
+        
+    }
+ }
