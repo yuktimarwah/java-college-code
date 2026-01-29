585349429d7657db7dec0efbfe648498d999ac55
Yukti <yuktimarwahgithub@gmail.com>
2026-01-29 23:16:24 +0530

    59th lec added

---
+++ lec_59_balanced_brackets/Balanced.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Balanced {
+    public static boolean isBalanced(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } 
+            else {
+                if (st.isEmpty()) {
+                    return false;
+                } 
+                else if (ch == ')' && st.peek() == '(') {
+                    st.pop();
+                } 
+                else if (ch == '}' && st.peek() == '{') {
+                    st.pop();
+                }
+                else if (ch == ']' && st.peek() == '[') {
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+                
+
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
