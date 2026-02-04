02f0e6ab036d566a5aae9ce159b541c7d10e5a39
Yukti <yuktimarwahgithub@gmail.com>
2026-02-04 20:10:54 +0530

    sem4 2nd lec added

---
+++ sem4_lec_2_fibo/fiboonacci.java
@@ -0,0 +1,11 @@
+public class fiboonacci {
+
+    public static int fibo(int n){
+        if(n<=1) return n;
+        return fibo(n-1) + fibo(n-2);
+    }
+    
+    public static void main (String [] args){
+        System.out.println(fibo(5));
+    }
+}
