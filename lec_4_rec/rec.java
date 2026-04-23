c253d43429e7bb90fe5d059e2816969714e25489
Yukti <yuktimarwahgithub@gmail.com>
2026-04-23 20:14:22 +0530

    sem4 lec4 added

---
+++ lec_4_rec/rec.java
@@ -0,0 +1,12 @@
+public class rec {
+    public static void print(int n){
+        if(n>3) return;
+        print(n+1);
+        System.out.println(n);
+        
+    }
+    public static void main(String[] args){
+        print(1);
+
+    }
+}
